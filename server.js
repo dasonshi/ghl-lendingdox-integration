41e8378a965f142a6aa80bbe31281ffdace6db36
David Sonshine <sonshine.david@gmail.com>
2025-08-08 10:34:45 -0500

    Add Nodemailer email alerts to replace EmailJS

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ function initializeEmailTransporter() {
     return;
   }
 
-  transporter = nodemailer.createTransporter({
+  transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: parseInt(process.env.SMTP_PORT) || 587,
     secure: process.env.SMTP_SECURE === 'true', // true for 465, false for other ports
